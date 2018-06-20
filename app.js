0561a9d2021e16e2f578d22744caa879de6bfd52
Creels <jeffkulzer90@gmail.com>
2018-06-20 13:19:04 -0400

    Changes to app.js

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ require('dotenv').load();
 const fs = require('fs');
 const path = require('path');
 
-// Constant for song folder location that shouldn't change
-const songFolder = 'E:\\SteamLibrary\\steamapps\\common\\Beat Saber\\CustomSongs';
-
 // Setup the options for the channel connection
 var options = {
   options: {
@@ -42,7 +39,7 @@ client.on('connected', function(address, port) {
 Repeat(followReminder).every(20, 'min').start.in(5, 'sec');
 
 function followReminder() {
-  client.action(process.env.COMM_CHANNEL, 'RaccAttack Don\'t forget to hit the follow button, it helps me a lot! RaccAttack')
+  client.action(process.env.COMM_CHANNEL, 'RaccAttack Don\'t forget to hit the follow button, it helps me a lot! RaccAttack');
 }
 
 // Respond to specific chat commands
@@ -52,12 +49,19 @@ client.on('chat', function(channel, user, message, self) {
       client.action(process.env.COMM_CHANNEL, 'twitter.com/dienter2');
       break;
     case '!songlist':
-      list = loadSongs(songFolder);
+      if (process.env.BEAT_SABER_COMMAND_ENABLED) {
+        console.log('!songlist is not enabled');
+        break;
+      }
+      list = loadSongs(process.env.BEAT_SABER_SONG_FOLDER);
       list.sort();
       console.log(list);
       break;
+    case (message.match(/^!/) || {}).input:
+      console.error(message + ' -- Recognized an attempted command that is not created.');
+      break;
     default:
-      console.log('Message not recognized');
+      console.log('Message: "' + message + '" not recognized');
       //client.action('dienter', user['display-name'] + ' this was a test')
   }
 });
@@ -70,7 +74,6 @@ var loadSongs = function(dir, filelist) {
   filelist = filelist || [];
   files.forEach(function(file) {
     if (path.join(dir, file).indexOf('.cache') > -1) {
-      console.log('YEP');
       return;
     } else if (fs.statSync(path.join(dir, file)).isDirectory()) {
       filelist = loadSongs(path.join(dir, file), filelist);
@@ -78,7 +81,7 @@ var loadSongs = function(dir, filelist) {
       try {
         var contents = fs.readFileSync(path.join(dir, file));
         var jsonContent = JSON.parse(contents);
-        //filelist.push(join(jsonContent.songName, ' - ', jsonContent.songSubName));
+        // If the array doesn't already exist, be sure to create it
         if (!Array.isArray(filelist)) {
           filelist = [];
         }
